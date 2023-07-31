3f980f0f15b0ff1b2de891054f049d0cb4666a00
Luke Dallenbach <lukedallenbach87@gmail.com>
2023-07-31 00:39:37 -0600

    i dont even know :/

--- adventure game.py
+++ adventure game.py
@@ -2,7 +2,7 @@
 def my_own_adventure_game():
     print('Hello!!! Thanks for trying this little game out yo!!!')
     user = input("What's your name yo?\n")
-    print(f'\nWelcome, {user} to this game.')
+    print(f'\nWelcome, {user}, to this game.')
     print('This game will be a little like a survial game, with each choice changing the outcome of the game.')
 
     
@@ -19,7 +19,7 @@ def my_own_adventure_game():
         user_character = input('Who would you like to be?\n')
         print(f'Awesome, so {user}, you chose {user_character}!')
         character_choices.remove(user_character)
-        print(f"So it's you and {', '.join(character_choices)} left in the game!")
+        print(f"So it's you, {', '.join(character_choices)} left in the game!")
         #the ', '.join() concatenates the remaining character names into a string separated by a ',' because i am modifying the original list
         print(f"Okay {user_character}, let's get started!!")
 
@@ -37,6 +37,20 @@ def my_own_adventure_game():
 
 
     characters_in_the_game()
+
+    def choice_1():
+        print('You and the others leave the room towards the hallway. Instead of seeing a hallway, there is a wall replacing the right side.')
+        print('The only direction to go is left.')
+        print('You and the others turn left and begin to walk. You are all greeted by a light shining into a room.')
+        print("The light illuminates the room to show a person tied up and yelling for help.")
+        print('Do you help them? ')
+        player_choice_1 = input('Which would you choose, yes or no?\n')
+        player_choice_1_lower = player_choice_1.lower()
+        if player_choice_1_lower == 'no':
+            print('This may come back to haunt you later.')
+        if player_choice_1_lower == 'yes':
+            print('This may help you in the future')
+    choice_1()
 
 
 my_own_adventure_game()
